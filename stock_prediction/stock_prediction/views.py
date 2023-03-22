3007992518c5b138c2e59b9db98d8cea8ebec80e
Cmeek09 <cnmeek@hotmail.com>
2023-03-21 18:21:06 -0600

    updated calendar and gather data page

--- stock_prediction/stock_prediction/views.py
+++ stock_prediction/stock_prediction/views.py
@@ -3,10 +3,22 @@ from django.shortcuts import HttpResponse
 
 
 # home page / landing page
+# home page / landing page
 def home_page(request):
-    return HttpResponse('<h1> Main Stock Page.<a href="/calendar/">Calendar</a>')
+    return HttpResponse('''
+        <h1>Main Stock Page</h1>
+        <ul>
+            <li><a href="/calendar/">Calendar</a></li>
+            <li><a href="/gatherData/">Pull stock features</a></li>
+        </ul>
+    ''')
 
 
 # claendar for finanacial dates
 def calendar_page(request):
     return render(request, 'calendar.html')
+
+
+# gather data page
+def getData_page(request):
+    return render(request, 'gatherData.html')
